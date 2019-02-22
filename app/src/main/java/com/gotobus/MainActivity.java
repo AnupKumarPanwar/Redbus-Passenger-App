c614d87f0ecc940f16ca774a85c17f77bcf3948d
Anup Kumar Panwar <1anuppanwar@gmail.com>
2019-02-23 00:46:22 +0530

    Nav drawer

--- app/src/main/java/com/gotobus/MainActivity.java
+++ app/src/main/java/com/gotobus/MainActivity.java
@@ -929,17 +929,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.trips) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.locate) {
 
-        } else if (id == R.id.nav_manage) {
+        }  else if (id == R.id.help) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout) {
 
         }
 
